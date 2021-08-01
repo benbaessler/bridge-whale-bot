d2466e0c3b818c141825b6bf1dbc0a515d5741ed
benbaessler <mail@benbaessler.com>
2021-08-01 14:56:32 +0200

    Added background tasks

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,49 @@
-import os
-import discord
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
+import discord
+import os
 
 load_dotenv()
 
-discord_channel_id = os.getenv('DISCORD_CHANNEL_ID')
+discord_channel_id = int(os.getenv('DISCORD_CHANNEL_ID'))
 discord_bot_token = os.getenv('DISCORD_BOT_TOKEN')
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '!')
+# channel = client.get_channel(discord_channel_id)
+
+class DiscordCog(commands.Cog):
+
+  def __init__(self, client):
+    self.client = client
+    self.send_onready_message.start()
+
+  def cog_unload(self):
+    self.send_onready_message.close()
+    return
+
+  @tasks.loop(count = 1)
+  async def send_onready_message(self):
+    channel = self.client.get_channel(int(discord_channel_id))
+    await channel.send('Hello!')
+
+  @send_onready_message.before_loop
+  async def before_send(self):
+    await self.client.wait_until_ready()
+    return
+
+  @send_onready_message.after_loop
+  async def after_send(self):
+    self.send_onready_message.close()
+    return
+
+class TaskRunner():
+  def __init__(self, client):
+    self.client = client
+
+  def run_tasks(self):
+    DiscordCog(self.client)
+    return
+
 
 @client.event
 async def on_ready():
@@ -17,7 +53,16 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user: return
 
+  # Testing
   if str(message.channel.id) == discord_channel_id:
     await message.channel.send('Hey {}'.format(message.author.name))
+
+async def background():
+  await client.wait_until_ready()
+  channel = client.get_channel(discord_channel_id)
+  await channel.send('Hello!')
+
+runner = TaskRunner(client)
+runner.run_tasks()
 
 client.run(discord_bot_token)
